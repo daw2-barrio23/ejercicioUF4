6d6db89931e65fe589569e1cb772ed3df902a9a7
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-04-28 17:16:42 +0000

    promesas acabado

--- promesas/main.js
+++ promesas/main.js
@@ -1,21 +1,21 @@
  //Exercici 2.1 Async/await
  
   async function pokeCards() {
-    const startTime = new Date().getTime(); // Tiempo de inicio
+    const inicioTiempo = new Date().getTime();
     try {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon");
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error("Fallo al conectar");
       }
       const data = await response.json();
       console.log(data);
-      const pokemonList = data.results.slice(0, 12); // Obtiene los primeros 12 pokémones
+      const listaPokemon = data.results.slice(0, 12);
       const pokemonArray = [];
   
-      for (const pokemon of pokemonList) {
+      for (const pokemon of listaPokemon) {
         const pokemonResponse = await fetch(pokemon.url);
         if (!pokemonResponse.ok) {
-          throw new Error("Network response for pokemon was not ok");
+          throw new Error("Fallo al conectar");
         }
         const pokemonData = await pokemonResponse.json();
         const pokemonInfo = {
@@ -28,17 +28,17 @@
         pokemonArray.push(pokemonInfo);
       }
   
-      // Mostrar los pokémons en pantalla
+      
       displayPokemon(pokemonArray);
       
     } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
+      console.error("Problema con la operacion:", error);
     } finally {
-      const endTime = new Date().getTime(); // Tiempo de finalización
-      const elapsedTime = endTime - startTime; // Calcular el tiempo transcurrido en milisegundos
-      const timeText = `TIEMPO: ${elapsedTime.toFixed(2)} milisegundos`; //Convierte el número a notación de punto fijo con digits decimales.
+      const finTIempo = new Date().getTime(); 
+      const elapsedTime = finTIempo - inicioTiempo; 
+      const timeText = `TIEMPO: ${elapsedTime.toFixed(2)} milisegundos`; 
       // Actualizar el texto del botón
-      const button = document.querySelector(".btn-primary"); // Puedes ajustar el selector según el botón correspondiente
+      const button = document.querySelector(".btn-primary"); 
       button.textContent = `Exercici 2.1 ${timeText}`;
     }
   }
@@ -48,7 +48,7 @@
   
     pokemonArray.forEach((pokemon) => {
       const cardColumn = document.createElement("div");
-      cardColumn.classList.add("col-md-3"); // Ajustar el tamaño de la columna según sea necesario
+      cardColumn.classList.add("col-md-3"); 
       cardColumn.innerHTML = `
               <div class="card shadow">
                   <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
@@ -71,34 +71,31 @@
   document.querySelector(".btn-primary").addEventListener("click", pokeCards);
   
   //Exercici 2.2 .then/.catch/.finally
-  // Fes el mateix exercici però fent servir .then/catch, ara encavalcant les peticions, de manera que els pokemos es mostrin en l'ordre correcte.
-  // Mostra al costat del botó el temps que s'ha fet servir.
-  // Actualitza la lògica perquè en prémer el botó corresponent es cridi aquesta funció i es mostri per pantalla els pokemons de l'array obtingut.
   
   function fetchPokemon() {
     
   
-    const startTime = new Date().getTime(); // Tiempo de inicio
+    const inicioTiempo = new Date().getTime(); 
   
     fetch("https://pokeapi.co/api/v2/pokemon")
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("Fallo al conectar");
         }
         return response.json();
       })
       .then((data) => {
-        const pokemonList = data.results.slice(0, 12); // Obtiene los primeros 12 pokémones
+        const listaPokemon = data.results.slice(0, 12); 
         const pokemonArray = [];
         console.log(pokemonArray);
-        // Encadenar las peticiones utilizando then encadenados
+        
         let chain = null;
-        pokemonList.forEach((pokemon) => {
+        listaPokemon.forEach((pokemon) => {
           if (chain === null) {
             chain = fetch(pokemon.url)
               .then((pokemonResponse) => {
                 if (!pokemonResponse.ok) {
-                  throw new Error("Network response for pokemon was not ok");
+                  throw new Error("Fallo al conectar");
                 }
                 return pokemonResponse.json();
               })
@@ -117,7 +114,7 @@
               return fetch(pokemon.url)
                 .then((pokemonResponse) => {
                   if (!pokemonResponse.ok) {
-                    throw new Error("Network response for pokemon was not ok");
+                    throw new Error("Fallo al conectar");
                   }
                   return pokemonResponse.json();
                 })
@@ -142,14 +139,14 @@
 
       })
       .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
+        console.error("Problema con la operacion:", error);
       })
       .finally(() => {
-        const endTime = new Date().getTime(); // Tiempo de finalización
-        const elapsedTime = endTime - startTime; // Calcular el tiempo transcurrido en milisegundos
-        const timeText = `TIEMPO: ${elapsedTime.toFixed(2)} milisegundos`; //Convierte el número a notación de punto fijo con digits decimales.
+        const finTIempo = new Date().getTime(); 
+        const elapsedTime = finTIempo - inicioTiempo; 
+        const timeText = `TIEMPO: ${elapsedTime.toFixed(2)} milisegundos`; 
         // Actualizar el texto del botón
-        const button = document.querySelector(".btn-success"); // Puedes ajustar el selector según el botón correspondiente
+        const button = document.querySelector(".btn-success"); 
         button.textContent = `Exercici 2.2 ${timeText}`;
       });
   }
@@ -157,31 +154,27 @@
   document.querySelector(".btn-success").addEventListener("click", fetchPokemon);
   
   // Exercici 2.3 Promise.All
-  // Fes el mateix exercici fent servir fetch i .then perquè totes les peticions es facin a l'hora.
-  // Utilitza Promise.all, de manera que les dades es mostrin per pantalla una vegada s'han resolt totes les peticions.
-  // Mostra el temps total que ha trigat.
-  // Actualitza la lògica perquè en prémer el botó corresponent es cridi aquesta funció i es mostri per pantalla els pokemons de l'array obtingut.
   function fetchPokeballParade() {
   
-    const startTime = new Date().getTime(); // Tiempo de inicio
+    const inicioTiempo = new Date().getTime(); 
   
     fetch("https://pokeapi.co/api/v2/pokemon")
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("Fallo al conectar");
         }
         return response.json();
       })
       .then((data) => {
-        const pokemonList = data.results.slice(0, 12); // Obtiene los primeros 12 pokémones
+        const listaPokemon = data.results.slice(0, 12); 
         const pokemonPromises = [];
   
-        // Crear un array de promesas para todas las peticiones de Pokémon
-        pokemonList.forEach((pokemon) => {
+        
+        listaPokemon.forEach((pokemon) => {
           pokemonPromises.push(fetch(pokemon.url)
             .then((pokemonResponse) => {
               if (!pokemonResponse.ok) {
-                throw new Error("Network response for pokemon was not ok");
+                throw new Error("Fallo al conectar");
               }
               return pokemonResponse.json();
             })
@@ -195,26 +188,20 @@
           );
         });
   
-        // Esperar a que todas las promesas se resuelvan
+        
         return Promise.all(pokemonPromises);
       })
       .then((pokemonArray) => {
-        // Mostrar los Pokémon en pantalla una vez se han resuelto todas las peticiones
         displayPokemon(pokemonArray);
-        // Borrar las fichas después de cierto tiempo (por ejemplo, 5 segundos)
-        
   
-        // Calcular el tiempo total transcurrido
-        const endTime = new Date().getTime();
-        const elapsedTime = endTime - startTime;
+        const finTIempo = new Date().getTime();
+        const elapsedTime = finTIempo - inicioTiempo;
         const timeText = `TIEMPO: ${elapsedTime.toFixed(2)} milisegundos`;
-  
-        // Actualizar el texto del botón
         const button = document.querySelector(".btn-warning");
         button.textContent = `Exercici 2.3 ${timeText}`;
       })
       .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
+        console.error("Problema con la operacion:", error);
       });
   }
   
